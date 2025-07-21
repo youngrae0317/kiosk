9b6c76d81f728fbb5f747d7cd57f85fa7afdf164
youngrae0317 <isp1229@naver.com>
2025-07-21 15:24:09 +0900

    ãlv3 kiosk 클래스 구현

---
+++ src/main/java/org/example/lv3/MenuItem.java
@@ -0,0 +1,39 @@
+package org.example.lv3;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Getter 메서드
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setter 메서드
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
