b704412a8058068ef409bcdbe7c074d0ffbd0f76
youngrae0317 <isp1229@naver.com>
2025-07-23 16:07:54 +0900

    스트림을 이용한 아이템 제거 구현

--- src/main/java/org/example/c_lv2/Order.java
+++ src/main/java/org/example/c_lv2/Order.java
@@ -33,4 +33,12 @@ public class Order {
     public boolean isEmpty() {
         return items.isEmpty();
     }
+
+    // 스트림을 이용한 아이템 제거
+    public void removeItem(String itemName) {
+        items.stream()
+                .filter(item -> item.getName().equals(itemName)) // itemName 매개변수와 동일한 아이템 필터
+                .findFirst() // 같은 이름의 첫 번째 아이템
+                .ifPresent(item -> items.remove(item)); // 존재할 경우에 리스트에서 제거!
+    }
 }
